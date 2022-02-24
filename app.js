2faff19d9e49fe99592c81b0bc02dbf6b45a3e55
Zachary Boelter <zboelter@gmail.com>
2022-02-24 16:27:21 -0600

    dom elements set to variables

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-// DOM Declarations
+// DOM Elements
 
 const lengthEl = document.getElementById('length');
-const generateEl = documant.getElementById('generate');
+const generateEl = document.getElementById('generate');
+const firstEl = document.getElementById('first-password');
+const secondEl = document.getElementById('second-password');
+const thirdEl = document.getElementById('third-password');
+const fourthEl = document.getElementById('fourth-password');
+
+console.log(fourthEl);
 
 
-
+// Get random element from array of all characters
 
 function getRandomChar() {
-    const characters = ["+", "-", "&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
+    const characters = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
         "~", "*", "?", ":", "\"", "\\", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     let randomIndex = Math.floor(Math.random() * characters.length);    
     return characters[randomIndex]

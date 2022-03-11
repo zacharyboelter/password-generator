941c6a5dd43c50fb038a5073903a8f0c2c18f6b2
Zachary Boelter <zboelter@gmail.com>
2022-03-11 15:51:37 -0600

    able to generate random password based on user input number

--- app.js
+++ app.js
@@ -7,22 +7,20 @@ const secondEl = document.getElementById('second-password');
 const thirdEl = document.getElementById('third-password');
 const fourthEl = document.getElementById('fourth-password');
 
-// console.log(fourthEl);
 
+const characters = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+        "~", "*", "?", ":", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // Get random element from array of all characters
 
-function getRandomChar() {
-    const characters = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
-        "~", "*", "?", ":", "\"", "\\", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    let randomIndex = Math.floor(Math.random() * characters.length);  
-    for (let i = 0; i < lengthEl.length; i++) {
-        return randomIndex[i]
+
+generateEl.addEventListener('click', () => {
+    let passwordLength = lengthEl.value 
+    let passWord = ''
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length) + 1;  
+        passWord += characters[randomIndex]
     }  
-    firstEl.textContent =  characters[randomIndex]
-
-}
-
-// console.log(getRandomChar());
-
-generateEl.addEventListener('click', getRandomChar())
+    firstEl.textContent = passWord; 
+})
